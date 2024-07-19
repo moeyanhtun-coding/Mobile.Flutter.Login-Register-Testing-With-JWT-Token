01703ad1f31bde11d31e6cb2dc37823200cfd1b7
Moe Yan Htun <moeyan227970@gmail.com>
2024-07-19 17:24:44 +0630

    [Add] Auth Services

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,46 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  User? _user;
+
+  User? get user {
+    return _user;
+  }
+
+  AuthService() {
+    _firebaseAuth.authStateChanges().listen(authStateChangesStreamLister);
+  }
+
+  Future<bool> Login(String email, String password) async {
+    try {
+      final credential = await _firebaseAuth.signInWithEmailAndPassword(
+          email: email, password: password);
+      if (credential.user != null) {
+        _user = credential.user;
+        return true;
+      }
+    } catch (e) {
+      print(e);
+    }
+    return false;
+  }
+
+  void authStateChangesStreamLister(User? user) {
+    if (user != null) {
+      _user = user;
+    } else {
+      _user = null;
+    }
+  }
+
+  Future<bool> logout() async {
+    try {
+      await _firebaseAuth.signOut();
+      return true;
+    } catch (e) {
+      print(e);
+    }
+    return false;
+  }
+}
